1775ffe3c16af4088e4ba4a14865cf2424da83d1
Speed <professionalcoder1337@gmail.com>
2023-12-10 22:23:34 +0530

    modified:   rand_meme_proto.php

--- rand_meme_proto.php
+++ rand_meme_proto.php
@@ -5,19 +5,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <style>
-                    .center
+            .center     /* for image, to be adjusted when merging */
             {
                 margin: auto;
                 width: 25%;    /* how much page width is occupied */
                 position: absolute;
-                top: 25%;
-                left: 40%;
+                top: 25%;   /* position from top */
+                left: 40%;  /* position from left */
                 padding: 10px;
             }
     </style>
 </head>
 <body>
     <?php
+    //      //unused code
     //def functions
     // function rand($x,$y)
     // {
@@ -31,22 +32,25 @@
     //     //echo $r_num;
     //     return $r_num;
     // }
-    function path($a,$b)
-    {
-        $str1="http://localhost/test1/";    
-        $str2=mt_rand($a,$b);
-        $str3=".jpg";
 
-        $str1.=$str2;
+    //when new memes added, update $b
+    function path($a,$b)        //$b: upper bound 
+    {
+        $str1="http://localhost/test1/";    //first part of address to be returned (home directory)
+        $str2=mt_rand($a,$b);               //generates a random number (file name)
+        $str3=".jpg";                       //extension to complete file path
+
+        $str1.=$str2;                       //concatenates all three strings above
         $str1.=$str3;
 
-        return $str1;
+        return $str1;                       //like c:\users\ak\fold\3.jpg
     }
     //   path(0,10);
 
     ?>
     <div class="center">
-    <img src="<?php echo path(0,10); ?>" alt="image goes brrr" height="250" width="250">
-</div>
+            <img src="<?php echo path(0,10); ?>" alt="image goes brrr" height="250" width="250">
+            <!-- php script is used to get a path containing a randomised image name -->
+    </div>
 </body>
 </html>
